51c448fea3ebef49f35df6edff3f5f3c8ca1c3dd
fabbernatvasvari <sze4fabber@vasvari.org>
2026-02-09 14:38:45 +0100

    ikonok hozzáadva a navbarhoz

--- frontend2/src/components/NavBar.vue
+++ frontend2/src/components/NavBar.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import { useAuthStore } from '../stores/auth'
 const auth = useAuthStore()
+
+// hozzáadjuk a pizzát a localStorage-hoz
+const localStorageItems = [...Object.keys(localStorage).map(key => ({ key, value: localStorage.getItem(key) }))];
+console.log('Local Storage Items:', localStorageItems);
+
 </script>
 
 <template>
   <nav class="navbar">
     <ul>
         <!-- rendes routerlinkek -->
-      <li><RouterLink to="/">Főoldal</RouterLink></li>
-      <li><RouterLink to="/restaurants">Éttermek</RouterLink></li>
-      <li><RouterLink to="/meals">Ételek</RouterLink></li>
-      <li><RouterLink to="/cart">Kosár</RouterLink></li>
-      <li>
-        <RouterLink to="/auth">
-          {{ auth.isLoggedIn
-            ? 'Fiók'
-            : auth.currentView === 'login'
-              ? 'Bejelentkezés'
-              : 'Regisztráció'
-          }}
-        </RouterLink>
-      </li>
+        <li>
+          <RouterLink to="/auth">
+            {{ auth.isLoggedIn
+              ? 'Fiók 👤'
+              : auth.currentView === 'login'
+                ? 'Bejelentkezés 🔐🤰'
+                : 'Regisztráció👨‍👨‍👦‍👦'
+            }}
+          </RouterLink>
+        </li>
+        <li><RouterLink to="/cart">🛒</RouterLink></li>
+        <li><RouterLink to="/meals">Ételek .|.</RouterLink></li>
+        <li><RouterLink to="/restaurants">Éttermek °|°</RouterLink></li>
+      <li><RouterLink to="/">🍕</RouterLink></li>
     </ul>
   </nav>
 </template>
