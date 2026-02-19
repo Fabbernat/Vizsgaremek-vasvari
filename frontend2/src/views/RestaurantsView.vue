948151309c27327b5ed5ca6a2e0db612ee638763
fabbernatvasvari <sze4fabber@vasvari.org>
2026-02-19 13:49:48 +0100

    hááát nem jelennek meg még mindig a restaurant ételei, de legalább a háttérfolyamatok jól elfutnak, elvileg

--- frontend2/src/views/RestaurantsView.vue
+++ frontend2/src/views/RestaurantsView.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-const restaurants = [
-  { name: 'Király Risztró', description: 'Finom és friss ételek várnak minden látogatót!', image: 'src/assets/pizza-margherita.jpg' },
-  { name: 'Pizza Mester', description: 'A legjobb pizzák a városban, friss alapanyagokból!', image: 'src/assets/hamburger.jpg' },
-  { name: 'Sushi Szamuráj', description: 'Autentikus japán sushi, amit imádni fogsz!', image: 'src/assets/caesar-salad.jpg' }
-]
+import { useRestaurantsStore } from '../stores/restaurants'
+
+const store = useRestaurantsStore()
 </script>
 
 <template>
   <h3>Ez az Éttermek oldal</h3>
 
   <ul>
-    <li v-for="restaurant in restaurants" :key="restaurant.name">
+    <li v-for="restaurant in store.restaurants" :key="restaurant.name">
       <strong>{{ restaurant.name }}</strong><br />
       {{ restaurant.description }}<br />
       {{ restaurant.image }}<br />
-      <button>Kiválasztás</button>
+      <button v-on:click="store.showRestaurant(restaurant)">Kiválasztás</button>
     </li>
   </ul>
 </template>
