6f2280663e1ad918d8a81b71dc9f911c47dfad62
fabbernatvasvari <sze4fabber@vasvari.org>
2026-02-19 13:56:45 +0100

    papíron elméletben az egész cart és rendelés logika működik

--- frontend2/src/views/MealsView.vue
+++ frontend2/src/views/MealsView.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
 import { useCartStore } from '../stores/cart'
+import { useMealsStore } from '../stores/meals'
 const cart = useCartStore()
-
-const meals = [
-  { name: 'Pizza', description: 'Sajt', price: 2500, image: 'img.png' },
-  { name: 'Hamburger', description: 'Hús', price: 3000, image: 'img.png' },
-  { name: 'Sushi', description: 'Hal', price: 4000, image: 'img.png' },
-  { name: 'Kóla', description: 'Szénsavas üdítő', price: 500, image: 'img.png' }
-]
+const store = useMealsStore()
 </script>
 
 <template>
   <h3>Ez az ételek oldal</h3>
 
   <ul>
-    <li v-for="meal in meals" :key="meal.name">
+    <li v-for="meal in store.meals" :key="meal.name">
     <li>{{ meal.name }}</li>
     <li>{{ meal.description }}</li>
-    <li>{{ meal.image }}</li>
+    <li>{{ meal.price }}</li>
     <button @click="cart.add({ name: meal.name, price: meal.price })">
       Kosárba tesz
     </button>
