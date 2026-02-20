d38fdeb7043702dcd79e43a60c5b9fb172cb35c9
fabbernatvasvari <sze4fabber@vasvari.org>
2026-02-20 16:10:25 +0100

    .app scope-ba raka az egész modern css

--- react-admin-felulet/src/App.tsx
+++ react-admin-felulet/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, type SetStateAction } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './ModernStyle.css'
 import { CurrentView } from './CurrentView';
 import {meals} from './stores/meals';
 import {restaurants} from './stores/restaurants';
