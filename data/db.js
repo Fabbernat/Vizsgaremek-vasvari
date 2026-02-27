eaac1c0fc678ab6ca792b687e34596da8420256b
Fabbernat <fabbernat@gmail.com>
2026-02-27 10:22:19 +0100

    most már a data\db.js képes létrehozni az adatbázissémát akkor is, ha az valamiért ellveszne

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 
 const db = new Database("./database.sqlite");
 
-const query = `
+const users = `
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
@@ -13,6 +13,46 @@ const query = `
     )
 `
 
-db.exec(query);
+db.exec(users);
+
+const restaurants = `
+    CREATE TABLE IF NOT EXISTS restaurants (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        ownerid INTEGER,
+        FOREIGN KEY(ownerid) REFERENCES owners(id)
+    )
+`
+db.exec(restaurants);
+
+const owners = `
+    CREATE TABLE IF NOT EXISTS owners (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+        password TEXT
+    )
+`
+db.exec(owners);
+
+const orders = `
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        userid INTEGER,
+        date TEXT,
+        FOREIGN KEY(userid) REFERENCES users(id)
+    )
+`
+db.exec(orders);
+
+const meals = `
+    CREATE TABLE IF NOT EXISTS meals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER
+    )
+`
+
+db.exec(meals);
 
 export default db;
