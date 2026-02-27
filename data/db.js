6d1fa7d699eea11c76054ea68af2e4824bedca7e
Fabbernat <fabbernat@gmail.com>
2026-02-27 10:07:11 +0100

    működik a data\db.js

--- data/db.js
+++ data/db.js
@@ -1,5 +1,18 @@
 import Database from "better-sqlite3";
 
-const db = new Database("./data/database.sqlite");
+const db = new Database("./database.sqlite");
+
+const query = `
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        firstName TEXT,
+        lastName TEXT,
+        email TEXT,
+        address TEXT
+    )
+`
+
+db.exec(query);
 
 export default db;
