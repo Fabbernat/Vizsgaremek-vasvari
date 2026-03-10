eae9410e8962dfa598bc6c46ab99357892862a49
Fabbernat <fabbernat@gmail.com>
2026-03-10 16:04:21 +0100

    nagyon jó verzió, erre érdemes visszatérni

--- react-admin-felulet/src/App.tsx
+++ react-admin-felulet/src/App.tsx
@@ -1,18 +1,38 @@
-// stílusok
 import './App.css'
+import { useState } from 'react';
 import { MainContent } from './MainContent'
 
+type ViewType = "blank" | "database";
+
 function App() {
+  const [activeView, setActiveView] = useState<ViewType>('blank');
+
+  function showDatabase() {
+    setActiveView('database');
+  }
+
   return (
     <>
-    <h1>Üdvözöljük a Royal Delivery admin felületén!</h1>
-    <h2>Válassza ki, hogy mit szeretne tenni</h2>
-      <ul>
-        <li><button> onClick={show} adatbázis adatainak megtekintése, kezelése </button></li>
-            <MainContent />
+      <h1>Üdvözöljük a Royal Delivery admin felületén!</h1>
+      <h2>Válassza ki, hogy mit szeretne tenni</h2>
+
+      <ul className='no-bullets'>
+        {activeView === "blank" ? (
+          <li>
+            <button onClick={showDatabase}>
+              Az adatbázis adatainak megtekintése, kezelése
+            </button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => setActiveView("blank")}>X</button>
+          </li>
+        )}
       </ul>
+
+      {activeView === "database" && <MainContent />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
