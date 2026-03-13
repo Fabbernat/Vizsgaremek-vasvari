9d19567660d9bbf5f521c00a4328cd21171eb0ae
Gakos29 <sz4gurako@vasvari.org>
2026-03-13 15:44:36 +0100

    Meals változtatás: Megcsináltam, hogy kitudjuk törölni az ételeket az éttermekből, majd visszadob az étterem ételeihez

--- react-frontend-akosnak/src/pages/OneMeal.tsx
+++ react-frontend-akosnak/src/pages/OneMeal.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import apiClient from "../api/apiClient";
 import type { Meals } from "../types/Meals";
-import { useParams } from "react-router-dom";
-import { Card, Container } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button, Card, Container } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 const OneMeal = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [meal, setMeal] = useState<Meals>();
 
@@ -16,17 +18,40 @@ const OneMeal = () => {
       .catch((error) => console.error(error));
   }, [id]);
 
+  //   Delete and Edit funcion
+  const handleDelete = () => {
+    apiClient
+      .delete(`/meals/${id}`)
+      .then(() => {
+        toast("Meal deleted successfully!", { type: "success" });
+        setMeal(undefined);
+        navigate(`/restaurants/${id}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast("Failed to delete meal.", { type: "error" });
+      });
+  };
+
   return (
     <>
       <Container>
         {meal ? (
-          <Card style={{ width: "25vw" }} data-bs-theme="dark">
+          <Card style={{ width: "20vw" }} data-bs-theme="dark">
             <Card.Body>
               <Card.Title>
                 <strong>{meal.name}</strong>
               </Card.Title>
               <Card.Text>{meal.description}</Card.Text>
               <Card.Text>Price: {meal.price}Ft</Card.Text>
+
+              {/* Delete and Edit Button */}
+              <Button variant="primary" className="me-3">
+                Edit
+              </Button>
+              <Button variant="danger">
+                <span onClick={handleDelete}>Delete</span>
+              </Button>
             </Card.Body>
           </Card>
         ) : (
