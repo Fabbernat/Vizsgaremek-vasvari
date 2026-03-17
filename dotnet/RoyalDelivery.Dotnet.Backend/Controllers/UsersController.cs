66f3a2cafac583cdee765bf2ac7ee62d3acc6bc7
Fabbernat <fabbernat@gmail.com>
2026-03-17 13:45:04 +0100

    userController api url-je fixálva

--- dotnet/RoyalDelivery.Dotnet.Backend/Controllers/UsersController.cs
+++ dotnet/RoyalDelivery.Dotnet.Backend/Controllers/UsersController.cs
@@ -11,7 +11,56 @@ namespace RoyalDelivery.Dotnet.Backend.Controllers
     {
         private readonly RoyaldeliveryDbContext _context = new();
 
-        [HttpGet("user/{username}")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _context.Users
+                .Select(u => new
+                {
+                    id = u.Id,
+                    username = u.Username,
+                    firstname = u.FirstName,
+                    lastname = u.LastName,
+                    email = u.Email,
+                    password = u.Password,
+                    address = u.Address
+                })
+                .ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound("Nincsenek felhasználók az adatbázisban!");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _context.Users
+                .Where(u => u.Id == id)
+                .Select(u => new
+                {
+                    id = u.Id,
+                    username = u.Username,
+                    firstname = u.FirstName,
+                    lastname = u.LastName,
+                    email = u.Email,
+                    password = u.Password,
+                    address = u.Address
+                })
+                .FirstOrDefaultAsync();
+
+            if (result == null)
+            {
+                return NotFound("Nem található a megadott azonosítójú felhasználó!");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{username}")]
         public async Task<IActionResult> GetByUsername(string username)
         {
             var result = await _context.Users.Where(u => u.Username == username).Select(u => new
