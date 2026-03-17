44b45cfce1128d6beb4f8a9f1efb6cd069bfebc6
Fabbernat <fabbernat@gmail.com>
2026-03-17 15:34:11 +0100

    Mindegyik controller létrehozva és működnek

---
+++ dotnet/RoyalDelivery.Dotnet.Backend/Controllers/RestaurantsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoyalDelivery.Dotnet.Backend.DbMysqlModels;
+
+namespace RoyalDelivery.Dotnet.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+        private readonly RoyaldeliveryDbContext _context = new();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _context.Restaurants.ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound("Nincsenek éttermek az adatbázisban!");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _context.Restaurants.FindAsync(id);
+
+            if (result == null)
+            {
+                return NotFound("Nem található a megadott azonosítójú étterem!");
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ dotnet/RoyalDelivery.Dotnet.Backend/Controllers/ShopsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoyalDelivery.Dotnet.Backend.DbMysqlModels;
+
+namespace RoyalDelivery.Dotnet.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+        private readonly RoyaldeliveryDbContext _context = new();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _context.Shops
+                .Select(s => new
+                {
+                    id = s.Id,
+                    name = s.Name
+                })
+                .ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound("Nincsenek boltok az adatbázisban!");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _context.Shops
+                .Where(s => s.Id == id)
+                .Select(s => new
+                {
+                    id = s.Id,
+                    name = s.Name
+                })
+                .FirstOrDefaultAsync();
+
+            if (result == null)
+            {
+                return NotFound("Nem található a megadott azonosítójú bolt!");
+            }
+
+            return Ok(result);
+        }
+    }
+}
