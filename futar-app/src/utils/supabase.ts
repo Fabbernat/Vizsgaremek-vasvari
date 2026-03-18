4948aea61a242f986deaffe275debb4fe41213b3
Fabbernat <fabbernat@gmail.com>
2026-03-18 16:44:25 +0100

    buildel végre

--- futar-app/src/utils/supabase.ts
+++ futar-app/src/utils/supabase.ts
@@ -1,17 +1,21 @@
 import { createClient, processLock } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'
 
-const AsyncStorage = require('@react-native-async-storage/async-storage').default
-
-export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-      lock: processLock,
-    },
-  })
+let AsyncStorage = undefined
+try {
+     AsyncStorage = require('@react-native-async-storage/async-storage').default
+     const supabase = createClient(
+       process.env.EXPO_PUBLIC_SUPABASE_URL!,
+       process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+       {
+         auth: {
+           storage: AsyncStorage,
+           autoRefreshToken: true,
+           persistSession: true,
+           detectSessionInUrl: false,
+           lock: processLock,
+         },
+       })
+} catch (error) {
+    
+}
