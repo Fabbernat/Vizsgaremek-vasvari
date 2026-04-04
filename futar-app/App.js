5be18264d530cb5a340cf3e46d7fe20f872ee081
Fabian Bernat <fabbernat@gmail.com>
2026-04-05 01:27:56 +0200

    homescreen javítva

--- futar-app/App.js
+++ futar-app/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Text>Royal Delivery futárszolgálat</Text>
       <Button title="Adatok megjelenítése a backendből" />
       <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
 }
