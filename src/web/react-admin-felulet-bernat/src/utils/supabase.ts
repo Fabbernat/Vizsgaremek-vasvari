cafa663ec63baef2507f7b2d5eed97e0388ec823
Fabbernat <fabbernat@gmail.com>
2026-04-23 10:59:52 +0200

    talán jobb lett

--- src/web/react-admin-felulet-bernat/src/utils/supabase.ts
+++ src/web/react-admin-felulet-bernat/src/utils/supabase.ts
@@ -1,8 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
+import process from 'process';
 
-const supabaseUrl = /*import.meta.env.VITE_SUPABASE_URL ||*/ 'https://uqusahmtbaypepskpxxa.supabase.co';
-const supabaseKey = /*import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY ||*/ 'sb_publishable_IAB2JpOGhKDJI7Sao5aXUw_C6Xh0lq-';
 
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://uqusahmtbaypepskpxxa.supabase.co';
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY || 'sb_publishable_IAB2JpOGhKDJI7Sao5aXUw_C6Xh0lq-';
+
+if (!supabaseUrl) {
+  throw new Error('Missing EXPO_PUBLIC_SUPABASE_URL');
+}
+if (!supabaseKey) {
+  throw new Error('Missing EXPO_PUBLIC_SUPABASE_KEY');
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
