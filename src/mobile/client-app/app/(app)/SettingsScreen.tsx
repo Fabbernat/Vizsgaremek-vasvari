b675c15bafc0f6c5f6b323aa2b6ebff75ae374db
Fabbernat <fabbernat@gmail.com>
2026-05-19 15:51:28 +0200

    updated elérési utak

--- src/mobile/client-app/app/(app)/SettingsScreen.tsx
+++ src/mobile/client-app/app/(app)/SettingsScreen.tsx
@@ -19,7 +19,7 @@ import {
 } from "react-native";
 import { supabase } from "../../supabase";
 import { useGlobalAuth } from "@/stores/AuthStore";
-import { useTheme } from "@/contextw/ThemeContext";
+import { useTheme } from "@/context/ThemeContext";
 
 // ─────────────────────────────────────────────────────────────────────────────
 // Sub-components
