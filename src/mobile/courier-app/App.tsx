7961efd60cfa40b060c1d3a57b6f47d7cdc08c69
fabbernat <fabbernat@gmail.com>
2026-05-21 01:29:15 +0200

    apróságok

--- src/mobile/courier-app/App.tsx
+++ src/mobile/courier-app/App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// courier-app/App.tsx
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ContactPreview from "./Contact";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ContactPreview />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
